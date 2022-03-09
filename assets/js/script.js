4d3dd5f9361908f04055d54825cc6ccb49bf7073
Yang Cheng <annacheng09@hotmail.com>
2022-03-08 19:35:59 -0800

    use data number to do color coding, still need some fixes for past

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,21 +13,23 @@ setInterval(updateTime, 1000);
 
 // checking what gives me the time in each p tag
 // console.log($('.time0').text())
-
 // do the color coding for past, present, and future
 for (let j=0; j< textSlots.length; j++) {
-    let currentTime = moment().format('h:mm a');
-    // checking if this logs current hour + am/pm
+    let dataNumber = $('.time' + j);
+    let currentTime = moment().format('H');
+    let compareTime = dataNumber.attr('data-number')
+    // checking if this logs current hour in 24
     // console.log(currentTime)
-    let compareTime = moment($('.time' + j).text())
-    if (moment(compareTime._i).isBefore(currentTime)) {
-        $('.text' + j).addClass('past')
-    } else if (moment(compareTime._i).isAfter(currentTime)) {
-        $('.text' + j).addClass('future')
-    } else {
+    // checking if it gives data number we set it up with
+    // console.log(compareTime)
+    if (compareTime === currentTime) {
         $('.text' + j).addClass('present')
+    } else if (compareTime < currentTime) {
+        $('.text' + j).addClass('past')
+    } else {
+        $('.text' + j).addClass('future')
     }
-    // console.log(compareTime._i)
+    console.log(compareTime + "<" + currentTime)
 }
 
 
